f2bf65c9b405896d38d512ea4e4a5cafbab17374
mbtzero <mtai@mywebfront.com>
2019-10-24 16:52:44 -0500

    ready to publish

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
-docker build -t mywebfront/multi-client -f ./client/Dockerfile ./client
-docker build -t mywebfront/multi-server -f ./server/Dockerfile ./server
-docker build -t mywebfront/multi-worker -f ./worker/Dockerfile ./worker
-docker push mywebfront/multi-client
-docker push mywebfront/multi-server
-docker push mywebfront/multi-worker
+docker build -t mywebfront/multi-client:latest -t mywebfront/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mywebfront/multi-server:latest -t mywebfront/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mywebfront/multi-worker:latest -t mywebfront/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
+docker push mywebfront/multi-client:latest
+docker push mywebfront/multi-server:latest
+docker push mywebfront/multi-worker:latest
+
+docker push mywebfront/multi-client:$SHA
+docker push mywebfront/multi-server:$SHA
+docker push mywebfront/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mywebfront/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mywebfront/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mywebfront/multi-worker:$SHA
