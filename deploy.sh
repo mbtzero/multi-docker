d2e9d7cbf4ed2d60c6c2caab9c4a81c4485ec852
mbtzero <mtai@mywebfront.com>
2019-10-25 16:18:34 -0500

    changed to gcp kube

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
-docker build -t mywebfront/multi-client:latest -t mywebfront/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t mywebfront/multi-server:latest -t mywebfront/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t mywebfront/multi-worker:latest -t mywebfront/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t mywebfront/angular:latest -t mywebfront/angular:$SHA -f ./angular/Dockerfile ./angular
+docker build -t mywebfront/backend:latest -t mywebfront/backend:$SHA -f ./backend/Dockerfile ./backend
 
-docker push mywebfront/multi-client:latest
-docker push mywebfront/multi-server:latest
-docker push mywebfront/multi-worker:latest
+docker push mywebfront/angular:latest
+docker push mywebfront/backend:latest
 
-docker push mywebfront/multi-client:$SHA
-docker push mywebfront/multi-server:$SHA
-docker push mywebfront/multi-worker:$SHA
+docker push mywebfront/angular:$SHA
+docker push mywebfront/backend:$SHA
 
-kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mywebfront/multi-server:$SHA
-kubectl set image deployments/client-deployment client=mywebfront/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=mywebfront/multi-worker:$SHA
+kubectl apply -f angk8s
+kubectl set image deployments/server-deployment server=mywebfront/backend:$SHA
+kubectl set image deployments/angular-deployment client=mywebfront/angular:$SHA
+
