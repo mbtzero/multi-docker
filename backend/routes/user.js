baf4b42dc036d3962bbc3f998aef151e9d281cb0
mbtzero <mtai@mywebfront.com>
2019-10-27 17:37:14 -0500

    update

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -34,7 +34,46 @@ router.get('/test/',(req,res, next) => {
 
 // Log user in
 router.post('/login/',(req,res, next) => {
-    res.status(201).json({message: 'signup successful 2'});
+
+
+  let fetchedUser;
+  User.findOne({ email: req.body.email})
+    .then(user => {
+      if (!user) {
+        console.log('could not find user for login')
+        return res.status(401).json({ message: 'failed'})
+      }
+
+      fetchedUser = user;
+
+      // compare passwords
+       enc =  hash(req.body.password);
+        console.log("user passed in password")
+        console.log(req.body.password)
+        console.log('comparing users ' + enc + ' against ' + user.password)
+
+        if (enc == user.password) return true;
+        return false;
+    })
+    .then(result=> {
+      if (result) {
+        const token = jwt.sign(
+          {email: fetchedUser.email, userId: fetchedUser._id},
+          'mikehasthispasshashturkeydogpizza',
+          { expiresIn: '1h'});
+        return res.status(200).json({message: 'user login success', token: token, expiresIn: 3600, userId: fetchedUser._id })
+      }
+      else
+      {
+        return res.status(401).json({ message: 'not authorized'})
+      }
+    })
+    .catch( error => {
+        return res.status(401).json({ message: 'unexpected err'})
+      console.log(error)
+    })
+
+    res.status(401).json({message: 'got here'});
 
 })
 
