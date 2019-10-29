e55552331140c0bacff8ea506d6f480409acc1cb
mbtzero <mtai@mywebfront.com>
2019-10-29 18:02:20 -0500

    cleanup:

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/banner.jpg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+import Home from "./components/home";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+
           <header className="App-header">
+            <div>
+              <div class="Header-title">
+                  MyGreenProfile
+              </div>
+
+            </div>
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+
           </header>
-          <div>
-            <Route exact path="/" component={Fib} />
+          <div style={{ margin: '32px'}}>
+            <Route exact path="/" component={Home} />
             <Route path="/react/otherpage" component={OtherPage} />
           </div>
         </div>
---
+++ client/src/components/home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Make an Impact!</h1>
+                <div>
+                    Do you want to make an impact on conservation and help to keep our planet green! MyGreenProfile allows you to track ways to conserve and to help teach others as well. Come back soon to find out more!
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Home;
